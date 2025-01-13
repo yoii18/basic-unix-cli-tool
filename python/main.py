8a381389e03e21e258204fe42a1681d424746dc4
Deepesh Salhotra <yoii186@Deepeshs-MacBook-Air.local>
2025-01-13 20:04:52 +0530

    added word count

--- python/main.py
+++ python/main.py
@@ -14,6 +14,16 @@ def file_size(files, size):
         res = f"{filesize} {file}"
         click.echo(res)
 
+def word_count(files):
+    for file in files:
+        words = 0
+        with open(file, 'r') as fp:
+            data = fp.read()
+            lines = data.split()
+            words += len(lines)
+        res = f"{words} {file}"
+        click.echo(res)
+
 @click.command()
 @click.argument('files', nargs=-1, type=click.Path(exists=True))
 @click.option(
@@ -28,11 +38,19 @@ def file_size(files, size):
     'get_lines',
     is_flag=True
 )
-def final(files, get_bytes, get_lines):
+@click.option(
+    '--word-count',
+    '-w',
+    'get_words',
+    is_flag=True
+)
+def final(files, get_bytes, get_lines, get_words):
     if get_bytes:
         file_size(files, size)
     if get_lines:
         line_count(files)
+    if get_words:
+        word_count(files)
 
 
 if __name__ == '__main__':
